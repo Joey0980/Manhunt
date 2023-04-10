3cc83d909b603531f9e14648388483230d8454c3
Joey <69287308+Joey0980@users.noreply.github.com>
2023-04-09 20:32:26 -0400

    Manhunt command will perform more checks and give the players a compass

--- src/main/java/systems/joey/manhunt/commands/ManhuntCommand.java
+++ src/main/java/systems/joey/manhunt/commands/ManhuntCommand.java
@@ -1,10 +1,13 @@
 package systems.joey.manhunt.commands;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import systems.joey.manhunt.Manhunt;
 
 public class ManhuntCommand implements CommandExecutor {
@@ -18,8 +21,20 @@ public class ManhuntCommand implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + "Please specify a player to start the manhunt");
         }
         if (sender instanceof Player player) {
-            plugin.setRunner(args[0]);
-            player.sendMessage(ChatColor.GREEN + "Manhunt Started with target " + args[0]);
+            if (args.length == 1) {
+                Player target = plugin.getServer().getPlayer(args[0]);
+                if (target == null) {
+                    sender.sendMessage(ChatColor.RED + "Player not found");
+                    return true;
+                }
+                plugin.setRunner(args[0]);
+                player.sendMessage(ChatColor.GREEN + "Manhunt Started with target " + args[0]);
+                for (Player onlinePlayer : plugin.getServer().getOnlinePlayers()) {
+                    if (onlinePlayer != target) {
+                        onlinePlayer.getInventory().addItem(new ItemStack(Material.COMPASS));
+                    }
+                }
+            } else return false;
         }
         return true;
     }
